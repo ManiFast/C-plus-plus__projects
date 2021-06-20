2a67dac15197ed90de52cf0cd047af46bc21835e
ManiFast <62830326+Stas-inside@users.noreply.github.com>
2021-06-20 15:32:53 +0500

    Add Digit_decomposition for HW

---
+++ All/Digit_decomposition__HW/Digit_decomposition__HW/Digit_decomposition__HW.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	int64_t x;
+	int sum = 0;
+	int count = 0;
+	std::cout << "Input value:" << std::endl;
+	std::cin >> x;
+	while (x > 0)
+	{
+		sum = sum + x % 10;
+		x /= 10;
+		count++;
+	}
+	printf("Sum = %d", sum);
+	std::cout << std::endl;
+	printf("Count = %d", count);
+	std::cout << std::endl << std::endl;
+	
+	system("pause");
+	return 0;
+}
