7f22dda23e1a164eaa073d1fd149e6f35341075f
ManiFast <62830326+Stas-inside@users.noreply.github.com>
2021-06-22 16:13:34 +0500

    Add lessons 47, 48

---
+++ Sergey_2/L47_Arg_in_function/L47_Arg_in_function/L47_Arg_in_function.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/* Аргументы по умолчанию */
+
+void foo(int a = 5, double b = 0.5)
+{
+	for (int i = 0; i < a; i++)
+	{
+		cout << "#";
+	}
+}
+
+int main()
+{
+	foo(4);
+
+
+	return 0;
+}
